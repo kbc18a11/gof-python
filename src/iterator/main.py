b185958c889bbb5cc3132f344a3f49936d6cd0ca
sarasaranuranura <wada121400@gmail.com>
2024-03-23 02:04:22 +0900

    演習問題完了

--- src/iterator/main.py
+++ src/iterator/main.py
@@ -20,33 +20,7 @@ class Book:
 
 @dataclass
 class BookSheif(Aggregate):
-    last: int
-    _books: list[Book] = field(default_factory=list)
-
-    def AddBook(self, book: Book):
-        """本を追加する
-
-        Args:
-            book (Book): 追加する本
-
-        Raises:
-            Exception: 本棚がに入れられないなら投げる
-        """
-        if self.last >= len(self._books):
-            self._books.append(book)
-        else:
-            raise Exception()
-
-    def FindBook(self, index: int) -> Book:
-        """指定された位置の本を返す
-
-        Args:
-            index (int): 指定位置
-
-        Returns:
-            Book: 指定された位置の本
-        """
-        return self._books[index]
+    books: list[Book] = field(default_factory=list)
 
     def Iterator(self) -> Iterator:
         """booksをスキャンするためのBookSheifIteratorインスタンスを返す
@@ -55,14 +29,6 @@ class BookSheif(Aggregate):
             Iterator: BookSheifIteratorインスタンス
         """
         return BookSheifIterator(self)
-
-    def getLength(self) -> int:
-        """本棚にある本の数を返す
-
-        Returns:
-            int: 本棚にある本の数
-        """
-        return len(self._books)
 
 
 @dataclass
@@ -76,7 +42,7 @@ class BookSheifIterator(Iterator):
         Returns:
             bool: True -> 存在している, False -> 存在していない
         """
-        return True if self.index < self.book_sheif.last else False
+        return True if self.index < len(self.book_sheif.books) else False
 
     def Next(self) -> any:
         """次の本を返す
@@ -87,17 +53,22 @@ class BookSheifIterator(Iterator):
         Returns:
             any: 次の本
         """
-        book = self.book_sheif.FindBook(self.index)
+        book = self.book_sheif.books[self.index]
         self.index += 1
         return book
 
 
 def main():
-    book_sheif = BookSheif(4)
-    book_sheif.AddBook(Book("a"))
-    book_sheif.AddBook(Book("b"))
-    book_sheif.AddBook(Book("c"))
-    book_sheif.AddBook(Book("d"))
+    book_sheif = BookSheif()
+    book_sheif.books.append(Book("a"))
+    book_sheif.books.append(Book("b"))
+    book_sheif.books.append(Book("c"))
+    book_sheif.books.append(Book("d"))
+    it = book_sheif.Iterator()
+    while it.HasNext():
+        book: Book = it.Next()
+        print(book.name)
+    book_sheif.books.append(Book("Z"))
     it = book_sheif.Iterator()
     while it.HasNext():
         book: Book = it.Next()
